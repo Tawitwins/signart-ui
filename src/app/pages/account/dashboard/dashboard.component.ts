b27adf73a55df8bdcf642e04c2458deda4005355
snfayem <>
2020-11-03 11:42:54 +0000

    commit

--- src/app/pages/account/dashboard/dashboard.component.ts
+++ src/app/pages/account/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { getAuthStatus } from 'src/app/auth/reducers/selectors';
+import { AppState } from 'src/app/interfaces';
+import { AuthServiceS } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,7 +14,7 @@ export class DashboardComponent implements OnInit {
 
   public openDashboard: boolean = false;
 
-  constructor() { }
+  constructor(private authService: AuthServiceS,  private store: Store<AppState>, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -17,5 +22,20 @@ export class DashboardComponent implements OnInit {
   ToggleDashboard() {
     this.openDashboard = !this.openDashboard;
   }
+
+  logout() {
+    this.authService.signOut();
+    this.redirectIfUserLoggedOut();
+    location.reload();
+ }
+ redirectIfUserLoggedOut(){
+  this.store.select(getAuthStatus).subscribe(
+    data => {
+      if (data === false) {
+      this.router.navigate(['home']);
+     }
+    }
+  );
+}
 
 }
