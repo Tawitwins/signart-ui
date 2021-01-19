6cf65b62b112489183ca0805b7b09992bb8e02a8
snfayem <>
2021-01-19 09:06:21 +0000

    commit

--- src/app/shop/collection/abonnement-catalogue/abonnement-catalogue.component.ts
+++ src/app/shop/collection/abonnement-catalogue/abonnement-catalogue.component.ts
@@ -693,7 +693,7 @@ export class AbonnementCatalogueComponent implements OnInit {
       cancelButtonColor: '#d33',
       confirmButtonText: 'OUI!'
     }).then((result) => {
-      if(result){
+      if(result == true){
         this.ngxService.startLoader("loader-01"); // start foreground spinner of the loader "loader-01" with 'default' taskId
         this.imageService.addAbonne(this.abonne).subscribe(
           respon =>{
@@ -773,7 +773,6 @@ export class AbonnementCatalogueComponent implements OnInit {
         this.montantTotal = 0;
         this.listeOeuvre = [];
         this.onGetImage();
-
       }
 
         });       
